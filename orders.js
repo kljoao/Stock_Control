de442345855718c20f6aeca66c9686e85f0ef4e1
João Ramos <joao.ramos@infocrerj.local>
2023-09-11 16:10:40 -0300

    first commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Sipag 3.0',
+        productNumber: '85743',
+        paymentStatus: 'Claro',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Sipag 3.0',
+        productNumber: '97245',
+        paymentStatus: 'Vivo',
+        status: 'Deletado'
+    },
+    {
+        productName: 'Sipag 2.0',
+        productNumber: '36452',
+        paymentStatus: 'Tim',
+        status: 'Active'
+    },
+]
